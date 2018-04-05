a84519d61a9315c4119a706b0bf1d2dd7934cb3a
AngeWa <angela.wang@sjsu.edu>
2018-04-05 13:06:41 -0700

    ye sbinch

---
+++ html/p5 part b/sketch.js
@@ -0,0 +1,27 @@
+let clouds;
+let x = 0;
+let speed = -5;
+
+function preload() {
+	clouds = loadImage('/assets/cloud.png');
+}
+
+function setup() {
+	createCanvas(500, 500);
+	console.log(clouds.width/2);
+}
+
+function draw() {
+	background(200);
+	image(clouds, x, 10, clouds.width / 2, clouds.height / 2)
+	move();
+}
+
+
+//function move()
+function move() {
+	x = x + speed;
+	if (x >= width || x < -225) {
+		speed = -speed
+	}
+}
