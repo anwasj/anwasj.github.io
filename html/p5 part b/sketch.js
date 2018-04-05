0a900724e508e14012e937f960d7ea35f4213d6a
AngeWa <angela.wang@sjsu.edu>
2018-04-05 13:24:00 -0700

    pleasse work
    ,,,,,

--- html/p5 part b/sketch.js
+++ html/p5 part b/sketch.js
@@ -3,18 +3,20 @@ let x = 0;
 let speed = -5;
 
 function preload() {
-	clouds = loadImage('assets/superMarioClouds_0.png');
+	clouds = loadImage('assets/cloud.png');
+	hand = loadImage ('assets/hand.png');
 }
 
 function setup() {
 		createCanvas(500, 500);
-		console.log(clouds.width/2);
+
 }
 
 function draw() {
 	background(200);
 	image(clouds, x, 10, clouds.width / 2, clouds.height / 2)
 	move();
+	image(hand, x, 220, handwidth / 1.5, hand.height / 1.5);
 }
 
 
